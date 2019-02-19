4e5e98ddc7a6e3a882f5f54b7ca4cf84339a4740
debryckethibault <thibault.debrycke@student.howest.be>
2019-02-19 10:59:47 +0100

    local storage finished

--- src/script/app.js
+++ src/script/app.js
@@ -1,21 +1,38 @@
+
+
 let countryHolder;
+let amountHolder;
 
 const localKey = "travel-planner";
 
 const hasItem = key => {
-    
+    // return ~getAllItems().indexOf(key);
+    return getAllItems().includes(key);
 };
 
 const addItem = key => {
-
+    let countries = getAllItems();
+    countries.push(key);
+    localStorage.setItem(localKey, JSON.stringify(countries));
 };
 
-const removeItem = key => {
+const getAllItems = () => {
+    return JSON.parse(localStorage.getItem(localKey)) || [];
+}
 
+const removeItem = key => {
+    const index = getAllItems().indexOf(key);
+    let savedCountries = getAllItems();
+    savedCountries.splice(index, 1);
+    localStorage.setItem(localKey, JSON.stringify(savedCountries));
+};
+
+const updateCounter = () => {
+    document.querySelector(".js-amount").innerHTML = countItems();
 };
 
 const countItems = () => {
-
+    return getAllItems().length;
 };
 
 
@@ -23,7 +40,14 @@ const addListenersToCountries = function(classSelector){
     const countries = document.querySelectorAll(classSelector);
     for (const country of countries){
         country.addEventListener("click", function(){
-            console.log("You clicked me ", this)
+            console.log("You clicked me ", this);
+            const countryKey = this.getAttribute("for");
+            if (hasItem(countryKey)){
+                removeItem(countryKey);
+            } else{
+                addItem(countryKey);
+            }
+            updateCounter();
         });
     }
 };
@@ -34,7 +58,7 @@ const showCounties = data => {
     for (const c of data) {
         countries += `
         <article>
-                    <input id="${c.cioc}-${c.alpha2Code}" class="o-hide c-country-input" type="checkbox">
+                    <input id="${c.cioc}-${c.alpha2Code}" class="o-hide c-country-input" type="checkbox" ${(hasItem(c.cioc + '-' + c.alpha2Code)) ? 'checked="checked"' : ''}>
                     <label for="${c.cioc}-${c.alpha2Code}" class="c-country js-country">
                     <div class="c-country-header">
                         <h2 class="c-country-header__name">${c.name}</h2>
@@ -71,8 +95,9 @@ const enableListeners = () => {
         });
     }
     countryHolder = document.querySelector(".js-country-holder");
-
+    amountHolder = document.querySelector(".js-amount")
     fetchCountries("europe");
+    updateCounter();
 };
 
 const init = () => {
